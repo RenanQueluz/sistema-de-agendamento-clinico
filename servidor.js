e868816e4e147f1d6e36dcb4557da108f4fc4184
RenanQueluz <renan.ag.queluz@gmail.com>
2026-03-22 18:20:56 -0300

    Primeiro commit

--- servidor.js
+++ servidor.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import mysql from 'mysql2/promise'
+import  dotenv from "dotenv";
+import jwt from "jsonwebtoken"
+
+dotenv.config();
+
+
 
 const app = express()
 app.use(express.json())
@@ -8,54 +14,82 @@ app.use(cors())
 
 //conexão mysql
 const bancoDados = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password:'16102006',
-    database: 'clinica_agendamento'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
+
+//armazenamento de teste
+const armazenamento = []
 
 //testando conexão
 try {
     const conexão = await bancoDados.getConnection();
     console.log('banco de dados conectado com sucesso')
 }
-catch (err){
+catch (err) {
     console.log('não foi possivel se conectar ao banco', err)
 }
 
 app.post('/login', async (req, res) => {
     try {
-        
-        const {pessoa, acesso} = req.body;
+        const { pessoa, acesso } = req.body;
         console.log(pessoa, acesso);
 
-        const sql = 'SELECT * FROM usuarios where nome = ? AND senha = ?'
-        
-        const [rows] = await bancoDados.execute(sql, [pessoa, acesso ])
+        const sql = 'SELECT * FROM usuarios where nome = ? AND cpf = ?'
 
+        const [rows] = await bancoDados.execute(sql, [pessoa, acesso])
         if (rows.length > 0) {
             res.status(200).json({
                 mensagem: 'usuario encontrado com sucesso'
             });
+
         } else {
             res.status(401).json({
                 mensagem: 'usuario ou senha incorretos'
             });
         }
-        
-    }catch (erro) {
+    } catch (erro) {
         console.error(erro);
     }
+
 })
 
 app.post('/cadastro', async (req, res) => {
 
+    try {
+        const { usuario, email, chave, sexo } = req.body
+
+        console.log(req.body)
+
+        const sql = 'INSERT INTO usuarios (nome, email, sexo, cpf) VALUES (?, ?, ?, ?)'
+
+        await bancoDados.execute(sql, [usuario, email, sexo, chave])
+
+        console.log(req.body)
+
+        res.status(201).json({
+            messagem: 'usuario criado com sucesso'
+        })
+
+    }
+
+    catch (error) {
+        console.log('erro em continuar')
+    }
+
+});
+
+app.post('/paciente', async (req, res) => {
+
+    armazenamento.push(req.body)
+
     res.status(201).json({
-        messagem: 'usuario cadastrado com sucesso'
+        mensagem: 'paciente criado',
+        dados: armazenamento
     })
 })
-
-
 
 app.listen(3000, () => {
     console.log('servidor rodando na porta 3000')
